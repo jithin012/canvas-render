fb7ae667677cbdefa9eb23040381ab7593f0eea5
jithin012 <jithin.m.baby@gmail.com>
2019-11-26 23:45:19 +0530

    Sculptor implemeation

--- src/App.js
+++ src/App.js
@@ -8,24 +8,45 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} id="logo" className="App-logo" alt="logo" />
-        <p>
-          Hello by CRA.
-        </p>
-        <button onClick={doCanvasOperation}>process canvas </button>
-      </header>
+        <div style={{display: 'flex'}}>
+          <section style={{display: 'flex', flexDirection: 'column', padding: '10px'}}>
+            <img src={logo} id="logo-local" className="App-logo" alt="logo" />
+            <button onClick={async () => {
+              const importUrl = await getBase64LocaImport();
+              document.getElementById("logo-local").src = importUrl;
+
+            }}>local import </button>
+          </section>
+
+          <section style={{display: 'flex', flexDirection: 'column', padding: '10px'}}>
+            <img src={logo} id="logo-url" className="App-logo" alt="logo" />
+            <button onClick={async () => {
+              const url = await getBase64UrlImport();
+              document.getElementById("logo-url").src = url;            
+            }}>url fetch </button>
+          </section>
+
+          <section style={{display: 'flex', flexDirection: 'column', padding: '10px'}}>
+            <img src={logo} id="logo-swatch" className="App-logo" alt="logo" />
+            <button onClick={async  () => {
+              const url =  getColorSwatchBase64String();
+              console.log(url);
+              document.getElementById("logo-swatch").src = url;
+            }}>canvas color swatch </button>
+          </section>
+        </div>
+    </header>
     </div>
   );
 }
- const doCanvasOperation = async ()=> {
-  const url = await getBase64()//.then(url => console.log(url))
-  console.log(url);
-  document.getElementById("logo").src = url;
-}
-function getBase64() {
+
+
+
+function getBase64LocaImport() {
   return new Promise(resolve => {
     const image = new Image();
     image.src = customlogo;
+
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
 
@@ -34,8 +55,55 @@ function getBase64() {
     image.onload = () => {
       context.drawImage(image, 0, 0);
       const url = canvas.toDataURL('image/png');
-      setTimeout(() => {resolve(url)},100)
+      resolve(url);
+    };
+
+  });
+}
+
+function getBase64UrlImport() {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.crossOrigin = "Anonymous";
+    image.src = 'https://avatars3.githubusercontent.com/u/933192?v=4';
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = image.width;
+    canvas.height = image.height;
+    image.onload = () => {
+      context.drawImage(image, 0, 0);
+      const url = canvas.toDataURL('image/png');
+      resolve(url);
     };
   });
+}
+
+function getColorSwatchBase64String() {
+  function borderPixels(pixelIndex, width, height) {
+    for (let i = 1; i <= 9; i += 1) {
+      if (pixelIndex === (width * 4 * i)) {
+        return true;
+      } else if (pixelIndex === ((width * 4 * i) - 4)) {
+        return true;
+      }
+    }
+    if ((pixelIndex < width * 4) || (pixelIndex >= ((width * height * 4) - (width * 4)))) {
+      return true;
+    }
+    return false;
+  }
+  
+  const colorArray = [255, 0, 0, 1];
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = 100;
+  canvas.height = 100;
+  const imgData = context.getImageData(0, 0, 50, 50);
+  context.fillStyle = "red";
+  context.fillRect(10, 10, 50, 50);
+  context.putImageData(imgData, 0, 0);
+  return canvas.toDataURL();
 }
 export default App;
