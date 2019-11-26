1d475ad75acc6c78d87aa8cec7ac8b3a0bd93d07
jithin012 <jithin.m.baby@gmail.com>
2019-11-26 21:55:04 +0530

    Updates

---
+++ precache-manifest.83eb4fe7d6008c9fed1fe7460564c286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e596e65f500c013d10fcfec58c46a1",
+    "url": "/canvas-render/index.html"
+  },
+  {
+    "revision": "985996cab082c669d5b6",
+    "url": "/canvas-render/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "efa25907c37a6d5dc488",
+    "url": "/canvas-render/static/js/2.226307dc.chunk.js"
+  },
+  {
+    "revision": "985996cab082c669d5b6",
+    "url": "/canvas-render/static/js/main.fd52d0f0.chunk.js"
+  },
+  {
+    "revision": "27b9d2b6bffe39cec590",
+    "url": "/canvas-render/static/js/runtime-main.84d1b5c9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/canvas-render/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "d054890aa6a20fe5273d24feff7acc79",
+    "url": "/canvas-render/static/media/logo.d054890a.jpg"
+  }
+]);
