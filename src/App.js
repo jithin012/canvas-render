a97cc68b43950854a4b2f80c8c7c7856688ecc76
jithin012 <jithin.m.baby@gmail.com>
2019-11-26 21:39:33 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import customlogo from './assets/logo.jpg';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} id="logo" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello by CRA.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={doCanvasOperation}>process canvas </button>
       </header>
     </div>
   );
 }
+ const doCanvasOperation = async ()=> {
+  const url = await getBase64()//.then(url => console.log(url))
+  console.log(url);
+  document.getElementById("logo").src = url;
+}
+function getBase64() {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.src = customlogo;
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
 
+    canvas.width = image.width;
+    canvas.height = image.height;
+    image.onload = () => {
+      context.drawImage(image, 0, 0);
+      const url = canvas.toDataURL('image/png');
+      setTimeout(() => {resolve(url)},100)
+    };
+  });
+}
 export default App;
